2fa64265011c1e0d36a032fb3e58a8ae03814462
kuchensheng <787211647@qq.com>
2018-08-20 01:09:17 +0800

    服务启动时，将服务注册到ZK

--- mermaid-core/src/main/java/com/mermaid/framework/config/ZkServiceConfiguration.java
+++ mermaid-core/src/main/java/com/mermaid/framework/config/ZkServiceConfiguration.java
@@ -1,6 +1,7 @@
 package com.mermaid.framework.config;
 
 import org.I0Itec.zkclient.ZkClient;
+import org.I0Itec.zkclient.serialize.SerializableSerializer;
 import org.I0Itec.zkclient.serialize.ZkSerializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -15,24 +16,23 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class ZkServiceConfiguration {
 
-    @Value("${mermaid.zk.servers:118.178.186:2181}")
+    @Value("${mermaid.zk.servers:118.178.186.33:2181}")
     private String zkServers;
 
-    @Value("${mermaid.zk.timeout.session:1000}")
+    @Value("${mermaid.zk.timeout.session:30000}")
     private int sessionTimeout;
 
-    @Value("${mermaid.zk.timeout.connection:3000}")
+    @Value("${mermaid.zk.timeout.connection:60000}")
     private int connectionTimeout;
 
     @Value("${mermaid.zk.channel_size:5}")
     private int channelSize;
 
-    @Value("${mermaid.zk.serialize.type:HessianSerializer}")
-    private ZkSerializer zkSerializer;
+
 
     @Bean
     public ZkClient zkClient() {
-        ZkClient zkClient = new ZkClient(zkServers,sessionTimeout,connectionTimeout,zkSerializer);
+        ZkClient zkClient = new ZkClient(zkServers,sessionTimeout,connectionTimeout);
         return zkClient;
     }
 }
