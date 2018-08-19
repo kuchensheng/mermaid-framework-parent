58fe30bb2f6ff91c2e7aaff10e83b855c784cfe2
kuchensheng <787211647@qq.com>
2018-08-19 22:33:50 +0800

    去除连接服务注册中心，改为由各个模块自己去实现

--- mermaid-core/src/main/java/com/mermaid/framework/MermaidApplicationEntry.java
+++ mermaid-core/src/main/java/com/mermaid/framework/MermaidApplicationEntry.java
@@ -2,16 +2,15 @@ package com.mermaid.framework;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
+
+import javax.annotation.Resource;
 
 
 @ComponentScan({"com.mermaid.framework"})
 @SpringBootApplication
-@EnableDiscoveryClient
-@EnableFeignClients
 public class MermaidApplicationEntry {
+
     public static void main(String[] args) {
         SpringApplication.run(MermaidApplicationEntry.class,args);
     }
