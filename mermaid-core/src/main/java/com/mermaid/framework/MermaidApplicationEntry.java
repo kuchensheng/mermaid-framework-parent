86a07097b15dd30de8af9720b37461168ea7d4a2
kuchensheng <787211647@qq.com>
2018-08-25 22:20:41 +0800

    服务注册和发现采用Eureka

--- mermaid-core/src/main/java/com/mermaid/framework/MermaidApplicationEntry.java
+++ mermaid-core/src/main/java/com/mermaid/framework/MermaidApplicationEntry.java
@@ -1,20 +1,32 @@
 package com.mermaid.framework;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ImportResource;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.SimpleCommandLinePropertySource;
+import org.springframework.util.StringUtils;
 
+import java.util.Properties;
 
-//@ComponentScan({"com"})
-//@EnableFeignClients
 @EnableDiscoveryClient
 @SpringBootApplication
+@EnableFeignClients
+@EnableCircuitBreaker
+@PropertySource(value = {"classpath:/META-INF/mermaid-eureka.properties","classpath:/META-INF/mermaid-module-framework-core.properties"})
+@ComponentScan("com")
 public class MermaidApplicationEntry {
 
     public static void main(String[] args) {
 
         SpringApplication.run(MermaidApplicationEntry.class,args);
+
+
     }
 }
--- mermaid-core/src/main/java/com/mermaid/framework/util/AnnotationUtil.java
+++
@@ -1,7 +0,0 @@
-package com.mermaid.framework.util;
-
-import java.util.Map;
-
-
-public class AnnotationUtil {
-}
