0203e4ad07afe2decdc1388cba316b672f25ce89
kuchensheng <787211647@qq.com>
2018-08-29 22:45:08 +0800

    添加Redis实现（字符串类型操作）

--- modules/mermaid-redis/src/main/java/com/mermaid/framework/redis/RedisCacheService.java
+++ modules/mermaid-redis/src/main/java/com/mermaid/framework/redis/RedisCacheService.java
@@ -1,23 +1,68 @@
 package com.mermaid.framework.redis;
 
+import java.util.Set;
+
 /**
  * @author Chensheng.Ku
  * 创建时间 2018-08-29 15:56
  * 描述：Redis缓存服务接口
  */
 public interface RedisCacheService {
+    /**
+     * 设置存储在给定键中的值
+     * @param key
+     * @param value
+     */
     void set(String key,Object value);
+
+    /**
+     * 设置存储在给定键中的值，并设置失效时间
+     * @param key
+     * @param value
+     * @param expire
+     */
     void set(String key,Object value,long expire);
 
-    String[] keys(String pattern);
+    /**
+     * 获取键的集合
+     * @param pattern
+     * @return
+     */
+    Set<String> keys(String pattern);
 
+    /**
+     * 获取给定键的字符串值
+     * @param key
+     * @return
+     */
     String get(String key);
 
-    boolean exists(String key);
-
+    /**
+     * 获取给定键的值，并自动转化为指定对象
+     * @param key
+     * @param tpl
+     * @param <T>
+     * @return
+     */
     <T> T get(String key,Class<T> tpl);
 
+    /**
+     * 判断给定键是否存在
+     * @param key
+     * @return
+     */
+    boolean exists(String key);
+
+    /**
+     * 删除键
+     * @param keys
+     */
     void delete(String... keys);
 
+    /**
+     * 设置失效时间
+     * @param key
+     * @param expire 失效时间，单位：秒（seconds）
+     */
     void expire(String key ,long expire);
 }
--- modules/mermaid-redis/src/main/java/com/mermaid/framework/redis/impl/DefaultRedisService.java
+++ modules/mermaid-redis/src/main/java/com/mermaid/framework/redis/impl/DefaultRedisService.java
@@ -1,56 +1,152 @@
 package com.mermaid.framework.redis.impl;
 
 import com.mermaid.framework.redis.RedisCacheService;
+import com.mermaid.framework.redis.RedisDistributedLockService;
 import com.mermaid.framework.redis.RedisService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.jackson.JsonObjectSerializer;
+import org.springframework.core.serializer.Serializer;
 import org.springframework.dao.DataAccessException;
 import org.springframework.data.redis.cache.RedisCache;
 import org.springframework.data.redis.connection.RedisConnection;
-import org.springframework.data.redis.core.RedisCallback;
-import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.*;
 import org.springframework.data.redis.serializer.RedisSerializer;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
 
 
-public class DefaultRedisService implements RedisCacheService {
+public class DefaultRedisService implements RedisCacheService,RedisDistributedLockService {
+
+    private static final Logger logger = LoggerFactory.getLogger(DefaultRedisService.class);
+    private static Map<String,Object> operationsMap = new HashMap<>();
+
+    private static final String STRING_TYPE="STRING_TYPE";
+    private static final String SET_TYPE="SET_TYPE";
+    private static final String HASH_TYPE="HASH_TYPE";
+    private static final String ZSET_TYPE="ZSET_TYPE";
+    private static final String LIST_TYPE="LIST_TYPE";
+    private static final String HYPERLOG_TYPE="HYPERLOG_TYPE";
+    /**
+     * 服务名
+     */
+    private String name;
+
+    private RedisTemplate redisTemplate;
+    public DefaultRedisService(String name) {
+        this(name,null);
+    }
+
+    public DefaultRedisService(String name, RedisTemplate redisTemplate) {
+        this.redisTemplate = redisTemplate;
+        this.setName(name);
+        initOperationsMap();
+    }
+
+    private void initOperationsMap() {
+        ValueOperations valueOperations = this.redisTemplate.opsForValue();
+        SetOperations setOperations = this.redisTemplate.opsForSet();
+        HashOperations hashOperations = this.redisTemplate.opsForHash();
+        HyperLogLogOperations hyperLogLogOperations = this.redisTemplate.opsForHyperLogLog();
+        ZSetOperations zSetOperations = this.redisTemplate.opsForZSet();
+        ListOperations listOperations = this.redisTemplate.opsForList();
+
+        operationsMap.put(STRING_TYPE,valueOperations);
+        operationsMap.put(SET_TYPE,setOperations);
+        operationsMap.put(HASH_TYPE,hashOperations);
+        operationsMap.put(ZSET_TYPE,zSetOperations);
+        operationsMap.put(HYPERLOG_TYPE,hyperLogLogOperations);
+        operationsMap.put(LIST_TYPE,listOperations);
+    }
 
     @Override
     public void set(String key, Object value) {
-
+        set(key,value,0l);
     }
 
     @Override
     public void set(String key, Object value, long expire) {
+        try {
+            if(null == value) {
+                delete(key);
+            }else {
+                ValueOperations operations = getOperation(STRING_TYPE);
+                operations.set(key,value,expire);
+            }
+        } catch (Exception e) {
+            logger.error("redis set error",e);
+        }
+    }
 
+    private ValueOperations getOperation(String stringType) {
+        return (ValueOperations) operationsMap.get(stringType);
     }
 
     @Override
-    public String[] keys(String pattern) {
-        return new String[0];
+    public Set<String> keys(String pattern) {
+        Set<String> keys = redisTemplate.keys(pattern);
+        return keys;
     }
 
     @Override
     public String get(String key) {
-        return null;
+        return get(key,String.class);
     }
 
     @Override
     public boolean exists(String key) {
-        return false;
+        return redisTemplate.hasKey(key);
     }
 
     @Override
     public <T> T get(String key, Class<T> tpl) {
-        return null;
+        if(!StringUtils.hasText(key)) {
+                return null;
+        }
+        ValueOperations operations = (ValueOperations) operationsMap.get(STRING_TYPE);
+        Object ret = operations.get(key);
+        return (T) ret;
     }
 
     @Override
     public void delete(String... keys) {
-
+        redisTemplate.delete(keys);
     }
 
     @Override
     public void expire(String key, long expire) {
+        redisTemplate.expire(key,expire, TimeUnit.SECONDS);
+    }
 
+    @Override
+    public boolean lock(String lockName) {
+        return false;
+    }
+
+    @Override
+    public boolean lock(String lockName, long expire) {
+        return false;
+    }
+
+    @Override
+    public void unlock(String lockName) {
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        if(!StringUtils.hasText(name)) {
+            name = DefaultRedisService.class.getSimpleName();
+        }
+        this.name = name;
     }
 }
