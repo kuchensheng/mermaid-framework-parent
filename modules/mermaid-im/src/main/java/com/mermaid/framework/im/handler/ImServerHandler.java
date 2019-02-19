c3a9ac1637a180cba97c006f589ebf447a08a413
chensheng.ku <chensheng.ku@geely.com>
2019-02-19 17:39:37 +0800

    fix

---
+++ modules/mermaid-im/src/main/java/com/mermaid/framework/im/handler/ImServerHandler.java
@@ -0,0 +1,43 @@
+package com.mermaid.framework.im.handler;
+
+
+import io.netty.channel.ChannelHandler;
+import io.netty.channel.ChannelHandlerAdapter;
+import io.netty.channel.ChannelHandlerContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author Chensheng.Ku
+ * @version 创建时间：2019/2/14 17:22
+ */
+@ChannelHandler.Sharable
+public class ImServerHandler extends ChannelHandlerAdapter{
+
+    private final static Logger logger = LoggerFactory.getLogger(ImServerHandler.class);
+
+    @Override
+    public void channelInactive(ChannelHandlerContext ctx) throws Exception {
+        super.channelInactive(ctx);
+    }
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        super.channelRead(ctx, msg);
+    }
+
+    @Override
+    public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
+        super.channelReadComplete(ctx);
+    }
+
+    @Override
+    public void userEventTriggered(ChannelHandlerContext ctx, Object evt) throws Exception {
+        super.userEventTriggered(ctx, evt);
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        super.exceptionCaught(ctx, cause);
+    }
+}
