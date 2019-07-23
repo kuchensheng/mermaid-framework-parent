7fdb0eb05388cb9fc6dfba8b8f84ebf1fc542913
kuchensheng <kuchensheng@songxiaocai.com>
2019-07-23 16:57:23 +0800

    去除build标签

--- modules/mermaid-core-dubbo/src/main/java/com/mermaid/framework/core/MermaidFrameworkEntry.java
+++ modules/mermaid-core-dubbo/src/main/java/com/mermaid/framework/core/MermaidFrameworkEntry.java
@@ -11,7 +11,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
