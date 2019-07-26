d3eb27f3a4c89d8ce3508b0bb9fc81305320cb09
kuchensheng <787211647@qq.com>
2019-07-26 21:02:33 +0800

    封装azkaban

---
+++ modules/mermaid-schedule/src/main/java/com/mermaid/framework/azkaban/AzkabanAdpater.java
@@ -0,0 +1,17 @@
+package com.mermaid.framework.azkaban;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Desription:
+ *
+ * @author:Hui CreateDate:2019/7/26 20:58
+ * version 1.0
+ */
+public class AzkabanAdpater {
+    private static final Logger logger = LoggerFactory.getLogger(AzkabanAdpater.class);
+
+
+
+}
